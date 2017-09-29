75b666a229eb575ac42198d4a6f518fb04f32c2d
Yochai Shavit <yochais@stanford.edu>
2017-09-29 15:51:30 -0700

    revert changes since 2nd commit

--- pset0.R
+++ pset0.R
@@ -1,19 +1,1 @@
 print("hello world")
-
-#Reasons to learn git
-reasons=c("I paid $2000 to recover data and analysis", "I make lots of changes, most of them are not good", 
-          "I like the freedom to explore", "I always remember to clean data only after running my analysis")
-#Print an affiration sentence
-reason=sample(reasons,1)
-need="I should learn git because"
-print(paste(need, reason))
-
-#do this 4 times at random
-learn_git= function (times){
-  for (i in times){
-  reason=sample(reasons,1)
-  output=print(paste(need, reason))
-  }
-}
-
-learn_git(c(1:4))
